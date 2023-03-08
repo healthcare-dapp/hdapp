c36f78e8303bff0b3eaeeae39dadba7f937cf50f
Ruslan Garifullin <ruslan.g2002@gmail.com>
2023-03-08 14:31:33 +0600

    fix: frontend base url

--- packages/js/web3/frontend/vite.config.ts
+++ packages/js/web3/frontend/vite.config.ts
@@ -1,25 +1,25 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  cacheDir: '../../../../node_modules/.vite/pwa-frontend',
+    cacheDir: "../../../../node_modules/.vite/pwa-frontend",
+    base: "./",
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+    server: {
+        port: 4200,
+        host: "localhost",
+    },
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
+    preview: {
+        port: 4300,
+        host: "localhost",
+    },
 
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: '../../../../',
-    }),
-  ],
+    plugins: [
+        react(),
+        viteTsConfigPaths({
+            root: "../../../../",
+        }),
+    ],
 });
