49c35bb62546ec29b5c5f59e9b16774f3aff5155
Ruslan Garifullin <ruslan.g2002@gmail.com>
2023-04-24 00:43:09 +0600

    fix: account manager address

--- packages/js/web3/frontend/src/contract.ts
+++ packages/js/web3/frontend/src/contract.ts
@@ -3,5 +3,5 @@
 export const WEB3_JSON_RPC_URL = "WDJFOaajRdjZqM82RoLrLLcq0Hhivrlh";
 
 export const HDMAccessControlAddress = "0x4b5E42D18a7f4c2C481Af2df962E5d6B0169506D";
-export const HDMAccountManagerAddress = "0xa27b0471E0332a6E5FD0ae3414E6cb1E6096725C";
+export const HDMAccountManagerAddress = "0x59e03D69F98D0Fa7Ba9e025C933b6CC0aB306177";
 export const HDMHandshakeAddress = "0xd4777F65870C6D7A1B0b7ed2B93189cAC7c3724E";
