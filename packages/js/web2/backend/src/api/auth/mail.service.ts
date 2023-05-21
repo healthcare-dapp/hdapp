76f5734b29df769032b1e822b942eae1ad5124db
A-Mironov <aamironov_1@edu.hse.ru>
2023-05-21 21:22:16 +0300

    MailFinish?

--- packages/js/web2/backend/src/api/auth/mail.service.ts
+++ packages/js/web2/backend/src/api/auth/mail.service.ts
@@ -212,11 +212,57 @@ export class MailService {
 
     async sendReviewNoticeEmail(userEmail: EmailAddress) {
         try {
+            // await this.mailer.sendMail({
+            //     to: userEmail, // list of receivers
+            //     subject: "HDAPP Registration Request on Review", // Subject line
+            //     text: "Thanks for applying for a doctor account on HDAPP!\r\nYour registration is currently on a review process by our moderation team. Once the review is over, you will be sent an e-mail to this address.", // plaintext body
+            // });
+
+            const html = readFileSync("review-begin.html", "utf8");
+
             await this.mailer.sendMail({
-                to: userEmail, // list of receivers
-                subject: "HDAPP Registration Request on Review", // Subject line
-                text: "Thanks for applying for a doctor account on HDAPP!\r\nYour registration is currently on a review process by our moderation team. Once the review is over, you will be sent an e-mail to this address.", // plaintext body
+                to: userEmail,
+                subject: "HDAPP Registration Request on Review",
+                html: html,
+                attachments: [
+                    {
+                        filename: "footer.png",
+                        path: "images/footer.png",
+                        cid: "footer",
+                    },
+                    {
+                        filename: "Companify-Logo.png",
+                        path: "images/Companify-Logo.png",
+                        cid: "Companify-Logo",
+                    },
+                    {
+                        filename: "Logo-white.png",
+                        path: "images/footer.png",
+                        cid: "logoWhite",
+                    },
+                    {
+                        filename: "facebook2x.png",
+                        path: "images/facebook2x.png",
+                        cid: "facebook2x",
+                    },
+                    {
+                        filename: "twitter2x.png",
+                        path: "images/twitter2x.png",
+                        cid: "twitter2x",
+                    },
+                    {
+                        filename: "instagram2x.png",
+                        path: "images/instagram2x.png",
+                        cid: "instagram2x",
+                    },
+                    {
+                        filename: "bee.png",
+                        path: "images/bee.png",
+                        cid: "bee",
+                    },
+                ],
             });
+
 
             debug("Sent registration review notice e-mail.", { email: userEmail });
         } catch (err) {
@@ -226,10 +272,55 @@ export class MailService {
 
     async sendReviewCompleteEmail(userEmail: EmailAddress) {
         try {
+            // await this.mailer.sendMail({
+            //     to: userEmail, // list of receivers
+            //     subject: "HDAPP Registration Request Completed", // Subject line
+            //     text: "Thanks for applying for a doctor account on HDAPP!\r\nWe are happy to inform you your registration request has been approved! You can now sign in to your account and start interacting with other users.", // plaintext body
+            // });
+
+            const html = readFileSync("review-complete.html", "utf8");
+
             await this.mailer.sendMail({
-                to: userEmail, // list of receivers
-                subject: "HDAPP Registration Request Completed", // Subject line
-                text: "Thanks for applying for a doctor account on HDAPP!\r\nWe are happy to inform you your registration request has been approved! You can now sign in to your account and start interacting with other users.", // plaintext body
+                to: userEmail,
+                subject: "HDAPP Registration Request Completed",
+                html: html,
+                attachments: [
+                    {
+                        filename: "footer.png",
+                        path: "images/footer.png",
+                        cid: "footer",
+                    },
+                    {
+                        filename: "Companify-Logo.png",
+                        path: "images/Companify-Logo.png",
+                        cid: "Companify-Logo",
+                    },
+                    {
+                        filename: "Logo-white.png",
+                        path: "images/footer.png",
+                        cid: "logoWhite",
+                    },
+                    {
+                        filename: "facebook2x.png",
+                        path: "images/facebook2x.png",
+                        cid: "facebook2x",
+                    },
+                    {
+                        filename: "twitter2x.png",
+                        path: "images/twitter2x.png",
+                        cid: "twitter2x",
+                    },
+                    {
+                        filename: "instagram2x.png",
+                        path: "images/instagram2x.png",
+                        cid: "instagram2x",
+                    },
+                    {
+                        filename: "bee.png",
+                        path: "images/bee.png",
+                        cid: "bee",
+                    },
+                ],
             });
 
             debug("Sent registration complete notice e-mail.", { email: userEmail });
